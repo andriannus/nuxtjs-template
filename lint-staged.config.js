fc58968727327911a7e230289f7ac02e2d5b0716
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-03-06 22:12:12 +0700

    chore: adjust lint-staged config

---
+++ lint-staged.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx,vue}": filenames => {
+    const onOneLine = filenames.join(" ");
+    return `npm run lint:script -- ${onOneLine}`;
+  },
+
+  "*.{css,scss,vue}": filenames => {
+    const onOneLine = filenames.join(" ");
+    return `npm run lint:style -- ${onOneLine}`;
+  },
+};
