9b20a518040ad57f8b030e5c394b75d6f3c5f456
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-09-22 09:44:44 +0700

    chore: update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,28 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-sass-guidelines",
+    "stylelint-config-suitcss",
+    "stylelint-config-prettier",
+  ],
+  plugins: ["stylelint-scss"],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    "max-nesting-depth": 3,
+    "string-quotes": "double",
+    "selector-class-pattern": null,
+    "selector-no-qualifying-type": [
+      true,
+      {
+        ignore: ["class"],
+      },
+    ],
+    "selector-pseudo-element-no-unknown": [
+      true,
+      {
+        ignorePseudoElements: ["v-deep"],
+      },
+    ],
+  },
 };
